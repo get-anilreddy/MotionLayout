f323c6ccd4276db8a91e7bf2844529c83d9cb7b4
cove_anilreddy <anil.reddy@coveiot.com>
2023-03-31 14:06:06 +0530

    Slide animation implemented

--- app/src/main/java/com/anil/motionlayout/MainActivity.kt
+++ app/src/main/java/com/anil/motionlayout/MainActivity.kt
@@ -2,10 +2,25 @@ package com.anil.motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anil.motionlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    var isTransitionToEnd = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAnimate.setOnClickListener {
+            if(!isTransitionToEnd) {
+                isTransitionToEnd = true
+                binding.motionLayout.transitionToEnd()
+            } else {
+                isTransitionToEnd = false
+                binding.motionLayout.transitionToStart()
+            }
+        }
     }
 }
